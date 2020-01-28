a1c64030d65f69add6c84eb6d3f2056757cf7969
davideoi <60237554+davideoi@users.noreply.github.com>
2020-01-28 18:28:20 +0100

    Afegit botó reset

--- checkingLists.js
+++ checkingLists.js
@@ -4,3 +4,17 @@ function eoi_tatxa(numero=100){
 	document.getElementsByClassName("linia")[numero].classList.toggle("arxivat")
 }
 
+function eoi_reset(){
+
+	const items = document.getElementsByClassName("linia")
+	const etiquetes = document.getElementsByTagName("label")
+	const nombreElements = items.length
+
+	if (nombreElements>0){
+		for(i=0;i<nombreElements;i++){
+			items[i].classList.remove("arxivat")
+			etiquetes[i].classList.remove("tatxat")
+		}
+	}
+}
+
